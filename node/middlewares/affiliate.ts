8ae3deb30c5614de097a1167228504e037352d2e
Filadelfo <filadelfo.braz@gmail.com>
2021-03-30 11:58:46 +0200

    Feat: Add log for (fake) API call responses

--- node/middlewares/affiliate.ts
+++ node/middlewares/affiliate.ts
@@ -3,11 +3,17 @@ import { json } from 'co-body'
 export async function affiliate(ctx: Context, next: () => Promise<any>) {
   const {
     clients: { glovo },
+    vtex: { logger },
   } = ctx
 
   const body = await json(ctx.req)
 
-  await glovo.sendBody(body)
+  try {
+    await glovo.sendBody(body)
+    logger.info(body)
+  } catch (error) {
+    throw new TypeError(error)
+  }
 
   ctx.status = 204
   await next()
