5f0f86aafba1044c251495fcd2dfcdd8421407eb
edyespinal <contacto@edyespinal.com>
2021-04-08 11:43:31 +0200

    feat: send order status change to beeceptor instead of logging tom console

--- node/middlewares/orderStates.ts
+++ node/middlewares/orderStates.ts
@@ -2,6 +2,26 @@ export async function orderStates(
   ctx: StatusChangeContext,
   next: () => Promise<any>
 ) {
-  console.log(ctx.body)
+
+  const {
+    body: { domain, orderId, currentState, currentChangeDate },
+    clients: { glovo },
+    vtex: { logger }
+  } = ctx
+
+  const glovoPayload = {
+    domain,
+    orderId,
+    currentState,
+    currentChangeDate
+  }
+
+  try {
+    await glovo.api(glovoPayload)
+    logger.info(glovoPayload)
+  } catch (error) {
+    throw new Error(error)
+  }
+
   await next()
 }
